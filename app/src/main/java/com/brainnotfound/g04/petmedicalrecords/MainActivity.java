d9214deb7768e5e8ef245e4fc251e987ee797efc
KCNT <korn.chinnatan@gmail.com>
2018-10-02 22:15:43 +0700

    Initial commit

---
+++ app/src/main/java/com/brainnotfound/g04/petmedicalrecords/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brainnotfound.g04.petmedicalrecords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
