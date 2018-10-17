f347a15b944175d5997728725cda45705156155f
KCNT <korn.chinnatan@gmail.com>
2018-10-17 20:33:34 +0700

    import from private account
    https://github.com/chinnatan/Pet-Medical-Records-2018

---
+++ app/src/main/java/com/brainnotfound/g04/petmedicalrecords/LoginFragment.java
@@ -0,0 +1,86 @@
+package com.brainnotfound.g04.petmedicalrecords;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.brainnotfound.g04.petmedicalrecords.module.SaveFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        checkCurrentUser();
+        initLoginBtn();
+    }
+
+
+    void getEmailAndPasswordToLogin() {
+        EditText _emailTxt = getView().findViewById(R.id.login_emailTxt);
+        EditText _passwordTxt = getView().findViewById(R.id.login_passwordTxt);
+
+        String _emailStr = _emailTxt.getText().toString();
+        String _passwordStr = _passwordTxt.getText().toString();
+
+        if(_emailStr.isEmpty() || _passwordStr.isEmpty()) {
+            Toast.makeText(getActivity(), "กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_SHORT).show();
+        } else if(_passwordStr.length() < 6) {
+            Toast.makeText(getActivity(), "กรุณากรอกรหัสผ่านมากกว่า 6 ตัวอักษร", Toast.LENGTH_LONG).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(_emailStr, _passwordStr).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    if(authResult.getUser().isEmailVerified()) {
+                        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new MenuFragment()).commit();
+                    } else {
+                        Toast.makeText(getActivity(), "กรุณายืนยัน email", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), "ERROR - " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    void checkCurrentUser() {
+        if(mAuth.getCurrentUser() != null) {
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new MenuFragment()).commit();
+        }
+    }
+
+    void initLoginBtn() {
+        Button _loginBtn = getView().findViewById(R.id.login_loginBtn);
+        _loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getEmailAndPasswordToLogin();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/brainnotfound/g04/petmedicalrecords/module/Profile.java
@@ -0,0 +1,59 @@
+package com.brainnotfound.g04.petmedicalrecords.module;
+
+public class Profile {
+
+    private String firstname = null;
+    private String lastname;
+    private String phonenumber;
+    private String account_type;
+
+    private static Profile profileInstance;
+
+    private Profile() {}
+
+    public Profile(String firstname, String lastname, String phonenumber, String account_type) {
+        this.setFirstname(firstname);
+        this.setLastname(lastname);
+        this.setPhonenumber(phonenumber);
+        this.setAccount_type(account_type);
+    }
+
+    public static Profile getProfileInstance() {
+        if(profileInstance == null) {
+            profileInstance = new Profile();
+        }
+        return profileInstance;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getAccount_type() {
+        return account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+}
