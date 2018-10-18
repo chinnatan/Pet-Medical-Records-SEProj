7f6a7b16a9e1e7ac4c8a3f7ffd49a34e1129cf95
KCNT <korn.chinnatan@gmail.com>
2018-10-18 20:01:56 +0700

    เลือกรูปภาพสัตว์เลี้ยง, แปลงรูปภาพเป็นแบบ rounded

--- app/src/main/java/com/brainnotfound/g04/petmedicalrecords/module/Pets/AddPetsFragment.java
+++ app/src/main/java/com/brainnotfound/g04/petmedicalrecords/module/Pets/AddPetsFragment.java
@@ -1,5 +1,9 @@
 package com.brainnotfound.g04.petmedicalrecords.module.Pets;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,14 +15,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
-import com.brainnotfound.g04.petmedicalrecords.MenuFragment;
 import com.brainnotfound.g04.petmedicalrecords.R;
+import com.brainnotfound.g04.petmedicalrecords.module.ImageConverter;
 
 import java.util.ArrayList;
 
 public class AddPetsFragment extends Fragment {
 
     private ArrayList<String> typeData = new ArrayList<String>();
+    private ArrayList<String> sexData = new ArrayList<String>();
+    private ArrayList<String> ageData = new ArrayList<String>();
+    private ImageView imageView;
 
     @Nullable
     @Override
@@ -30,19 +37,75 @@ public class AddPetsFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Spinner spinnerType = getView().findViewById(R.id.frg_menu_pets_add_type);
+        imageController();
+        setDataInSpinner();
+        initBackBtn();
+    }
 
-        createTypeData();
+    private void imageController() {
+        imageView = getView().findViewById(R.id.frg_menu_pets_add_images);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "กรุณาเลือกรูปภาพสัตว์เลี้ยงของคุณ"), 1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == getActivity().RESULT_OK) {
+            try {
+                Uri uri = data.getData();
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap circleBitmap = ImageConverter.getRoundedConnerBitmap(bitmap, 150);
+                imageView.setImageBitmap(circleBitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void createData() {
+        typeData.add("สุนัข");
+        typeData.add("แมว");
+
+        sexData.add("ผู้");
+        sexData.add("เมีย");
+
+        ageData.add("1 เดือน");
+        ageData.add("2 เดือน");
+        ageData.add("3 เดือน");
+        ageData.add("4 เดือน");
+        ageData.add("5 เดือน");
+        ageData.add("6 เดือน");
+        ageData.add("7 เดือน");
+        ageData.add("8 เดือน");
+        ageData.add("9 เดือน");
+        ageData.add("10 เดือน");
+        ageData.add("11 เดือน");
+        ageData.add("12 เดือน");
+    }
+
+    private void setDataInSpinner() {
+        Spinner spinnerType = getView().findViewById(R.id.frg_menu_pets_add_type);
+        Spinner spinnerSex = getView().findViewById(R.id.frg_menu_pets_add_sex);
+        Spinner spinnerAge = getView().findViewById(R.id.frg_menu_pets_add_age);
+
+        createData();
 
         ArrayAdapter<String> adapterType = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, typeData);
         spinnerType.setAdapter(adapterType);
 
-        initBackBtn();
-    }
+        ArrayAdapter<String> adapterSex = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, sexData);
+        spinnerSex.setAdapter(adapterSex);
 
-    private void createTypeData() {
-        typeData.add("สุนัข");
-        typeData.add("แมว");
+        ArrayAdapter<String> adapterAge = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, ageData);
+        spinnerAge.setAdapter(adapterAge);
     }
 
     void initBackBtn() {
